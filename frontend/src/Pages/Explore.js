45533f3c832e52dcb5ba062305e40f80925796c3
Tanvi Roy <tanvi.roy_ug22@ashoka.edu.in>
2020-11-08 14:04:18 +0530

    Made project structure

---
+++ frontend/src/Pages/Explore.js
@@ -0,0 +1,19 @@
+// Explore/Search Page
+
+import React, { Component } from "react";
+import "../App.css";
+
+class Explore extends Component {
+  
+    render() {
+      return (
+        <div>
+        
+            <h1>Explore Page</h1>
+  
+        </div>
+      );
+    }
+  }
+
+export default Explore;
---
+++ frontend/src/Pages/Hotel.js
@@ -0,0 +1,19 @@
+// Individual Hotel Page
+
+import React, { Component } from "react";
+import "../App.css";
+
+class Hotel extends Component {
+  
+    render() {
+      return (
+        <div>
+        
+            <h1>Individual Hotel Page</h1>
+  
+        </div>
+      );
+    }
+  }
+
+export default Hotel;
