2058b27398cdc8cf4ccfbb206691cb059f66a56a
Tanvi Roy <tanvi.roy_ug22@ashoka.edu.in>
2020-11-20 17:04:45 +0530

    Made Hotel Page

--- frontend/src/Pages/Hotel.js
+++ frontend/src/Pages/Hotel.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from "react";
 import "../App.css";
+import "../styles/hotel.css";
 import { Container } from "react-bootstrap";
 import NavbarComp from "../components/navbar";
 
@@ -30,27 +31,53 @@ class Hotel extends Component {
 
     <div style={{paddingBottom: "5%"}}>
 
-        <div className="header">
+    {this.state.hotels.map((hotel) =>(
+      <li key={hotel.name}>
+        
+        <div className="hotel-banner">
           <NavbarComp />
-        </div>
-      <div className="product-container">
-
-        <div className="item-b">
-          <Container id="content">
-          {this.state.hotels.map((hotel) =>(
-            <li key={hotel.name}>
-              <div>{hotel.name}</div><br />
-              <div>{hotel.location}</div><br />
-              <div><p>${hotel.price}</p></div><br />
-              <div><p style={{fontWeight: "600"}}>Product Description:</p>
-                {hotel.desc}</div><br />
-            </li> 
-          ))}
-          </Container>
-
-          </div>
+          <img
+            className="ihotel-img"
+            src={hotel.imageurl}
+            alt="hotel"
+          />
         </div>
 
+        <svg className="circle" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="55" cy="55" r="55" fill="white" />
+            <text x="32" y="64" fill="black" fontSize="xx-large" position="absolute">{hotel.rating}</text>
+        </svg>
+
+
+        <div className="container4">
+
+         <div className="left4">
+          <div style={{fontSize: "3rem", color: "#003060"}}>{hotel.name}</div><br />
+          <div style={{color: "grey", marginTop: "-1%", position: "relative"}}>{hotel.location}</div><br />
+          <div><p>${hotel.price}</p></div><br />
+          <div><p style={{fontWeight: "600"}}>Hotel Description:</p>
+          {hotel.desc}</div><br />
+          <div><p style={{fontWeight: "600"}}>Hotel Amenities:</p>
+            <ul>{hotel.amenities.map(name => <li style={{listStyleType: "disc"}} key={name}> {name} </li>)}</ul>
+          </div><br />
+
+          <br />
+
+          <button>Book Now</button> &nbsp; &nbsp; &nbsp;
+          <button>Add to Bucket List</button>
+          
+         </div>
+
+         <div className="right4">
+         ⚠️ Map goes here!
+         </div>
+
+        </div>
+      
+      </li> 
+        
+        ))}
+          
     </div>
     );
   }
